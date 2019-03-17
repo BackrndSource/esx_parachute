a5f00581d01bb793960775b28766b0cb35980658
BackrndSource <back.rndsource@gmail.com>
2019-03-17 13:48:35 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,8 @@
+Locales['en'] = {
+  ['parachute_jump'] = 'parachute jump',
+  ['in_jump'] = 'You jumped!',
+  ['destination_point'] = 'destination',
+  ['cant_finish_jump'] = 'you can´t finish...',
+  ['jump'] = 'press ~INPUT_CONTEXT~ to jump!',
+  ['arrive'] = 'press ~INPUT_CONTEXT~ to finish'
+}
